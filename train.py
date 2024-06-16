8ae26e13ebfd31bfeece902cbf3d2b6299e9ab16
Abhishek Baghel <122452418+abhishekbaghel11@users.noreply.github.com>
2024-06-16 20:42:19 +0530

    Update train.py

--- train.py
+++ train.py
@@ -35,37 +35,37 @@ def train(data_path, lr , weights, epochs, validation_split, validation_path, sa
 
     # set the train as well as the validation dataset
     if((validation_split == None) and (validation_path == None)):
-        train_low = sorted(glob(os.path.join(data_path,"low/*")))
-        train_enhanced = sorted(glob(os.path.join(data_path,"high/*")))
+        train_low = sorted([file for file in glob(os.path.join(data_path,"low/*")) if not file.split('/')[-1].startswith('.')])
+        train_enhanced = sorted([file for file in glob(os.path.join(data_path,"high/*")) if not file.split('/')[-1].startswith('.')])
         train_dataset = create_dataset(train_low, train_enhanced, batch_size)
 
         val_dataset = None
 
     elif(validation_path != None):
-        train_low = sorted(glob(os.path.join(data_path,"low/*")))
-        train_enhanced = sorted(glob(os.path.join(data_path,"high/*")))
+        train_low = sorted([file for file in glob(os.path.join(data_path,"low/*")) if not file.split('/')[-1].startswith('.')])
+        train_enhanced = sorted([file for file in glob(os.path.join(data_path,"high/*")) if not file.split('/')[-1].startswith('.')])
         train_dataset = create_dataset(train_low, train_enhanced, batch_size)
 
-        val_low = sorted(glob(os.path.join(validation_path,"low/*")))
-        val_enhanced = sorted(glob(os.path.join(validation_path,"high/*")))
+        val_low = sorted([file for file in glob(os.path.join(validation_path,"low/*")) if not file.split('/')[-1].startswith('.')])
+        val_enhanced = sorted([file for file in glob(os.path.join(validation_path,"high/*")) if not file.split('/')[-1].startswith('.')])
         val_dataset = create_dataset(val_low, val_enhanced, batch_size)
     
     elif(validation_split != None):
         if(validation_split > 100 or validation_split < 0):
             raise ValueError("The value of the validation split is not in the required range, please keep it between 0-100")
         
-        MAX_TRAIN_IMAGES = len(glob(os.path.join(data_path,"low/*")))
+        MAX_TRAIN_IMAGES = len([file for file in glob(os.path.join(data_path,"low/*")) if not file.split('/')[-1].startswith('.')])
         MAX_TRAIN_IMAGES = MAX_TRAIN_IMAGES - int(MAX_TRAIN_IMAGES * (validation_split/100))
 
-        train_low = sorted(glob(os.path.join(data_path,"low/*")))[:MAX_TRAIN_IMAGES]
-        train_enhanced = sorted(glob(os.path.join(data_path,"high/*")))[:MAX_TRAIN_IMAGES]
+        train_low = sorted([file for file in glob(os.path.join(data_path,"low/*")) if not file.split('/')[-1].startswith('.')])[:MAX_TRAIN_IMAGES]
+        train_enhanced = sorted([file for file in glob(os.path.join(data_path,"high/*")) if not file.split('/')[-1].startswith('.')])[:MAX_TRAIN_IMAGES]
         train_dataset = create_dataset(train_low, train_enhanced, batch_size)
 
         if(validation_split == 0):
             val_dataset = None
         else:
-            val_low = sorted(glob(os.path.join(data_path,"low/*")))[MAX_TRAIN_IMAGES:]
-            val_enhanced = sorted(glob(os.path.join(data_path,"high/*")))[MAX_TRAIN_IMAGES:]
+            val_low = sorted([file for file in glob(os.path.join(data_path,"low/*")) if not file.split('/')[-1].startswith('.')])[MAX_TRAIN_IMAGES:]
+            val_enhanced = sorted([file for file in glob(os.path.join(data_path,"high/*")) if not file.split('/')[-1].startswith('.')])[MAX_TRAIN_IMAGES:]
             val_dataset = create_dataset(val_low, val_enhanced, batch_size)
 
     # start the training
@@ -92,7 +92,7 @@ def train(data_path, lr , weights, epochs, validation_split, validation_path, sa
     print("The model has been trained and the weights are saved")
 
     # plot the graphs
-    if plot_data != False:
+    if plot_data != 0:
         plot_graph(history, "loss", "Loss", save_weights_path)
         plot_graph(history, "peak_signal_noise_ratio", "PSNR", save_weights_path)
 
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     parser.add_argument('--validation_split', type = int, default = None, help = 'Defines the split for the validation data in the training data')
     parser.add_argument('--validation_data', type = str, default = None, help = 'Path to the validation data (overwrites the validation split argument if any path is given)')
     parser.add_argument('--save_weights_path', type = str, default = None, help= 'Path to the folder where to save the weights and the plots after training')
-    parser.add_argument('--plot_data', type = bool, default = False, help = 'Show the plot of the data or not (data --> losses and the psnr)')
+    parser.add_argument('--plot_data', type = int, default = 0, help = 'Show the plot of the data or not (data --> losses and the psnr)')
     parser.add_argument('--batch_size', type = int, default= 4, help = 'Define the batch size')
 
     args = parser.parse_args()
